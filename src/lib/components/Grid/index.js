3a4cee8ffb772c11c2834a22313416c812f8588b
segun-jidonu <segun.jidonu@stream4.tech>
2023-11-30 10:41:52 +0000

    reverse date formatter

--- src/lib/components/Grid/index.js
+++ src/lib/components/Grid/index.js
@@ -235,7 +235,10 @@ const GridBase = memo(({
             if (column.linkTo) {
                 overrides.cellClassName = "mui-grid-linkColumn";
             }
-            finalColumns.push({ headerName: column.headerName || column.label, ...column, ...overrides });
+            finalColumns.push({ headerName: column.headerName || column.label, valueGetter: (params) => {
+                const newField = column.field;
+                return column.type === 'time' ? dayjs(params.row[newField]).format('LT') :  params.row[newField]
+            }, ...column, ...overrides });
             if (column.pinned) {
                 pinnedColumns[column.pinned === 'right' ? 'right' : 'left'].push(column.field);
             }
